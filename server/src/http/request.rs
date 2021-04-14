84981f259604edafb4d4f8f88f8f6aff877966fe
Ba Nyar Naing <ba.n.naing@frontiir.net>
2021-04-14 16:22:12 +0630

    parse header

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,5 +1,6 @@
 use super::method::{Method, MethodError};
 use super::QueryString;
+use super::HeaderString;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -10,6 +11,7 @@ use std::str::Utf8Error;
 pub struct Request<'buf> {
     path: &'buf str,
     query_string: Option<QueryString<'buf>>,
+    header_string: Option<HeaderString<'buf>>, 
     method: Method,
 }
 
@@ -25,6 +27,10 @@ impl<'buf> Request<'buf> {
     pub fn query_string(&self) -> Option<&QueryString> {
         self.query_string.as_ref()
     }
+
+    pub fn header_string(&self) -> Option<&HeaderString> {
+        self.header_string.as_ref()
+    }
 }
 
 impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
@@ -36,7 +42,7 @@ impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
@@ -50,9 +56,13 @@ impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
             path = &path[..i];
         }
 
+        let mut header_string = Some(HeaderString::from(request));
+        println!("{:?}", header_string);
+
         Ok(Self {
             path,
             query_string,
+            header_string,
             method,
         })
     }
